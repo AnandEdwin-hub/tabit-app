d9817eda95577f4fe526d9012daa425a315ab49e
AnandEdwin-hub <anandedwinn@gmail.com>
2025-11-01 13:57:17 +0530

    Configure for PythonAnywhere

--- Tabit/app.py
+++ Tabit/app.py
@@ -272,6 +272,5 @@ def delete_group(group_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    # For production (Render)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run()
+
